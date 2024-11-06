4a5e17b71af45ed1ff6ff9f0189e7c2f03975244
Katalyst99 <katleho333@gmail.com>
2024-11-06 16:11:24 +0000

    Task 3 correction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -40,7 +40,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     pdb_pwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     pdb_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     pdb_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    pdb_connection = mysql.connector.connect(
+    pdb_connection = mysql.connector.connection.MySQLConnection(
         user=pdb_user,
         password=pdb_pwd,
         host=pdb_host,
@@ -66,3 +66,17 @@ class RedactingFormatter(logging.Formatter):
         """
         msg = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
+
+def main() -> None:
+    """
+    Obtains a database connection using get_db,
+    and retrieve all rows in the users table,
+    and display each row under a filtered format
+    """
+    pdb = get_db()
+    cursor = pdb.cursor()
+
+    qry = "SELECT * FROM users;"
+    cursor.execute(qry)
+    names = [
