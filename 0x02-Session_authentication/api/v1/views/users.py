12fadd9263528931d09195211644ad38c4276d89
Katalyst99 <katleho333@gmail.com>
2024-11-13 16:10:05 +0000

    Trying a different implementation

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,7 +27,7 @@ def view_one_user(user_id: str = None) -> str:
     """
     if not user_id or user_id == 'me' and not request.current_user:
         abort(404)
-    if user_id == 'me' and request.current_user:
+    else:
         return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
