1dcd4a73e1ba2292c734046ba942477a3cae7dc3
Katalyst99 <katleho333@gmail.com>
2024-11-21 07:50:39 +0000

    Improvement to task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -95,6 +95,7 @@ class Auth:
         except NoResultFound:
             raise ValueError
         user.reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=user.reset_token)
         return user.reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
