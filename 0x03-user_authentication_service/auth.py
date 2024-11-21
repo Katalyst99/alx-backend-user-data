b928ba39224f1151a74354abcd7b2bf4d14800de
Katalyst99 <katleho333@gmail.com>
2024-11-21 08:24:02 +0000

    Improvement to task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -109,5 +109,5 @@ class Auth:
             raise ValueError
         hashed_password = _hash_password(password)
         self._db.update_user(user.id, hashed_password=hashed_password,
-                             reset_token=Nome)
+                             reset_token=None)
         return None
